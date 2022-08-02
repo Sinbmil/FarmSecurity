b38c8d794a0fcf90135e68235d24a61a8c55a547
Lim Byeong Chan <qud902@naver.com>
2022-08-02 16:58:30 +0900

    Update Camera.java

--- restapi/src/main/java/com/farmsecurity/restapi/model/Camera.java
+++ restapi/src/main/java/com/farmsecurity/restapi/model/Camera.java
@@ -1,12 +1,10 @@
 package com.farmsecurity.restapi.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name ="camera")
