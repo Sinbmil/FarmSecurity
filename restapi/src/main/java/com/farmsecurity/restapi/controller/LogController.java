51c0eb56f8835266036f6fccabf92afe2ea7813d
Lim Byeong Chan <qud902@naver.com>
2022-08-08 16:15:58 +0900

    update

--- restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
+++ restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
@@ -1,11 +1,13 @@
 package com.farmsecurity.restapi.controller;
 
 import com.farmsecurity.restapi.model.Camera;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.farmsecurity.restapi.model.Log;
 import com.farmsecurity.restapi.repository.LogRepository;
 import com.farmsecurity.restapi.repository.CameraRepository;
+import com.farmsecurity.restapi.firebase.FCMService;
 
 import java.util.*;
 
@@ -19,11 +21,14 @@ public class LogController {
     @Autowired
     private CameraRepository cameraRepository;
 
+    private FCMService fcmservice;
+
     @PostMapping("/insert") // CREATE
-    public Log insert(@RequestBody Map<String, String> map){
+    public Log insert(@RequestBody Map<String, String> map) throws FirebaseMessagingException {
         List<Camera> camera = cameraRepository.findByCameraNum(map.get("cameraNum"));
         if(camera.size() == 1){
             map.put("cameraName",camera.get(0).getCameraName());
+            fcmservice.sendMessage("cAn4cdFJQImXCx2o0zj3Qa:APA91bEfwsYFh1sV78d-VKpAzOcjD-OyCJlREQirIMy12Ua82C2Ukho_XGjGd6gQW7g5RdMshnyJFhqngqkQDh7e4p70HjElaVSEkyLu60OBkVRmicQpvXs-uWSMqBCt-8Gi36z5oSre");
              return logRepository.save(
                      new Log(map.get("cameraNum"), map.get("cameraName"), map.get("link"), map.get("level"), map.get("time"))
              );
