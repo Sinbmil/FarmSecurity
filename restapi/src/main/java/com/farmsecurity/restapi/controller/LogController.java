a5c6798205dc0fac6ba2fc0ac6c7565c930eb2b1
Lim Byeong Chan <qud902@naver.com>
2022-08-12 17:37:54 +0900

    update

--- restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
+++ restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
@@ -39,10 +39,10 @@ public class LogController {
 
         if(camera.size() == 1){
             map.put("cameraName",camera.get(0).getCameraName());
-            map.put("id", member_id.get(0).getId());
+            map.put("id", member_id.get().getId());
             fcm.sendMessageTo(member.get(0).getToken(),"알림","현재 농장의 상태를 확인해주세요");
              return logRepository.save(
-                     new Log(map.get("cameraNum"), map.get("cameraName"), map.get("link"), map.get("level"), map.get("time"))
+                     new Log(map.get("id"), map.get("cameraNum"), map.get("cameraName"), map.get("link"), map.get("level"), map.get("time"))
              );
         } else{
             throw new IllegalStateException("카메라가 존재하지 않습니다.");
