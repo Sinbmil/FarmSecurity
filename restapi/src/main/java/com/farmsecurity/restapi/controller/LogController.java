a61e4c26b9f44f94abb8fc32de9d3d41235c08db
Lim Byeong Chan <qud902@naver.com>
2022-08-15 22:49:46 +0900

    update

--- restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
+++ restapi/src/main/java/com/farmsecurity/restapi/controller/LogController.java
@@ -37,7 +37,7 @@ public class LogController {
             map.put("memberId", camera.get(0).getId());
             // List<Member> member = memberRepository.findByToken(map.get("memberId"));
             // String s = member.get(0).getToken();
-            fcm.sendMessageTo("clZ1uXFKSrm7vIVnkYUJXf:APA91bFQwzSMaE4pquKNnW-XB3aO_xMOrAhYMkxNXmreaKarn2rRZQoCdhEXnHpl6buQvqYSrM5LdG06-uBBduPLJyTKmPa4-noNtljMW9uslyaZim3a6MheW5PDYYZNQ4xldoOcuy3G","알림","현재 농장의 상태를 확인해주세요");
+            fcm.sendMessageTo("cR3lt95YTmCfiizfxNdrKi:APA91bF7hXsPDiso9H32lr1JPhGI-LCfdCHPMh-A0r-Vwz9pD7xpFiOBhUdn510dovO83hinIUXQx15pAnzU-2ipr4-_UtKKcepnLoap_wZ8Qkn0Ws33WPdYwcJQiVicHZZZ1HStf1VW","알림","현재 농장의 상태를 확인해주세요");
             // System.out.println("what the" + s);
              return logRepository.save(
                      new Log(map.get("memberId"), map.get("cameraNum"), map.get("cameraName"), map.get("link"), map.get("level"), map.get("time"))
