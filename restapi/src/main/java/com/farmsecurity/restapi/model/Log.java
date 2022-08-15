bce02f1f490ab232d756448436eb51b9b3b18ecb
Lim Byeong Chan <qud902@naver.com>
2022-08-15 21:13:20 +0900

    update

--- restapi/src/main/java/com/farmsecurity/restapi/model/Log.java
+++ restapi/src/main/java/com/farmsecurity/restapi/model/Log.java
@@ -15,8 +15,8 @@ public class Log { // 로그 테이블
     @Column(name="num")
     private long num;
 
-    //@JoinColumn(name="member_id")
-    // private String memberId;
+    @JoinColumn(name="member_id")
+    private String memberId;
     @JoinColumn(name="camera_cameraNum")
     private String cameraNum;
     private String cameraName;
@@ -24,8 +24,8 @@ public class Log { // 로그 테이블
     private String level;
     private String time;
 
-    public Log(String cameraNum, String cameraName, String link, String level, String time){
-        // this.memberId = memberId;
+    public Log(String memberId, String cameraNum, String cameraName, String link, String level, String time){
+        this.memberId = memberId;
         this.cameraNum =cameraNum;
         this.cameraName = cameraName;
         this.link = link;
--- restapi/src/main/java/com/farmsecurity/restapi/repository/LogRepository.java
+++ restapi/src/main/java/com/farmsecurity/restapi/repository/LogRepository.java
@@ -1,7 +1,6 @@
 package com.farmsecurity.restapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.farmsecurity.restapi.model.Log;
 
@@ -10,6 +9,6 @@ import java.util.List;
 @Repository
 public interface LogRepository extends JpaRepository<Log, String> { // 로그 레포지토리
 
-    // List<Log> findByMemberId(String memberId); // 로그 찾기
+    List<Log> findByMemberId(String memberId); // 로그 찾기
 
 }
