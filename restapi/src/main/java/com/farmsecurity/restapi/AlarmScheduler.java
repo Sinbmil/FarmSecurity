7504dcf4bb41760f865d42196c4cfd1678878372
Lim Byeong Chan <qud902@naver.com>
2022-08-18 23:37:39 +0900

    Update AlarmScheduler.java

--- restapi/src/main/java/com/farmsecurity/restapi/AlarmScheduler.java
+++ restapi/src/main/java/com/farmsecurity/restapi/AlarmScheduler.java
@@ -32,7 +32,7 @@ public class AlarmScheduler {
      */
 
     //매일 0시 1분에 날짜 체크
-    @Scheduled(cron = "0 36 * * * * ")
+    @Scheduled(cron = "0 39 * * * * ")
     public void DailyCheck(){
         System.out.println("실행1");
         int idx = 0;
@@ -55,6 +55,7 @@ public class AlarmScheduler {
         LocalDate timeDate2 = timeDate.plusMonths(1);
         LocalDate todayDate = LocalDate.from(today);
         if(timeDate.isEqual(todayDate)){
+            System.out.println("실행4");
             logRepository.deleteByTime(String.valueOf(time));
         }
     }
