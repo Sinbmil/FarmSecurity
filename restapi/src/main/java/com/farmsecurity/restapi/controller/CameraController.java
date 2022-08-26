79fbf1a69d78a611620cc96eb723f43b021b6bbe
Lim Byeong Chan <qud902@naver.com>
2022-08-26 17:19:08 +0900

    Update CameraController.java

--- restapi/src/main/java/com/farmsecurity/restapi/controller/CameraController.java
+++ restapi/src/main/java/com/farmsecurity/restapi/controller/CameraController.java
@@ -1,6 +1,5 @@
 package com.farmsecurity.restapi.controller;
 
-import com.farmsecurity.restapi.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.farmsecurity.restapi.model.Camera;
